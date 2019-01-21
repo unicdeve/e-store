15190b527e99be15c73da4f887dd1fcac778a161
Taiwo Ogunola <uniqueomokenny@gmail.com>
2019-01-21 20:01:45 +0100

    implemented handleDetail for the Detail product

--- src/context.js
+++ src/context.js
@@ -27,9 +27,17 @@ class ProductProvider extends Component {
     });
   }
 
-  handleDetail = () => {
-    console.log("Hello from detail");
+  getItem = (id) => {
+    const product = this.state.products.find(item => item.id === id);
+    return product;
   }
+
+  handleDetail = (id) => {
+    const product = this.getItem(id);
+    this.setState(() => {
+      return {detailProduct:product}
+    });
+  };
 
   addToCart = (id) => {
     console.log(`Hello from addToCart, id is ${id}`);
