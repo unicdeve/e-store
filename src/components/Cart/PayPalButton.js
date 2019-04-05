55a1914e2c777508401826c52e612c9441584230
Taiwo Ogunola <uniqueomokenny@gmail.com>
2019-04-05 23:18:44 +0100

    Invalid client ID

--- src/components/Cart/PayPalButton.js
+++ src/components/Cart/PayPalButton.js
@@ -30,7 +30,7 @@ export default class PayPalButton extends React.Component {
         // Document on Paypal's currency code: https://developer.paypal.com/docs/classic/api/currency_codes/
  
         const client = {
-            sandbox: process.env.REACT_APP_APP_ID,
+            sandbox: 'AbDkZEvGtZpLgo5QaoMiSBF713qfQeqx1MsFUiUBuMUGKYTC8SjcgwrGdwQIuVeiSCAG7a_GuopKB2Se',
             production: 'YOUR-PRODUCTION-APP-ID',
         }
         // In order to get production's app-ID, you will have to send your app to Paypal for approval first
